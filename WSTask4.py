5a9e1441cb6a4b14db47ed0da54372615a072faa
DenizzG <64283539+DenizzG@users.noreply.github.com>
2020-09-11 10:33:23 +0100

    Create WSTask4.py

---
+++ WSTask4.py
@@ -0,0 +1,7 @@
+Nst = int(input("How many students are there? = "))
+Nbk = int(input("How many books are there? = "))
+
+Books_per_s = int(Nst % Nbk)
+remaining = Nbk %Nst
+
+print("Each student gets " , Books_per_s, " books and there are ", remaining, " books left over.")
